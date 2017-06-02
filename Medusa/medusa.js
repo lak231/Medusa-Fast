51647d6ce17719aaa0ef129354734e0d9962109f
khaiquangnguyen <kqn001@bucknell.edu>
2017-06-02 15:06:29 -0400

    done

--- Medusa/medusa.js
+++ Medusa/medusa.js
@@ -8,19 +8,6 @@ function off() {
     document.getElementById("consent-form").style.display = "none";
 }
 
-function start_webgazer(){
-    webgazer.setRegression('ridge') 
-  	    .setTracker('clmtrackr')
-  	    .setGazeListener(function(data, elapsedTime) {
-            if (data == null) {
-                return;
-            }
-            var xprediction = data.x; //these x coordinates are relative to the viewport 
-            var yprediction = data.y; //these y coordinates are relative to the viewport
-            console.log(elapsedTime); //elapsed time is based on time since begin was called
-        })
-    	.begin()
-}
 
 window.onbeforeunload = function() {
     webgazer.end(); 
