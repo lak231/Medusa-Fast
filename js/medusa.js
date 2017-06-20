eda1c2ad2984a19ddcdcbc88208c1e6c508cf116
khaiquangnguyen <kqn001@bucknell.edu>
2017-06-20 15:52:39 -0400

    done

--- js/medusa.js
+++ js/medusa.js
@@ -138,6 +138,52 @@ freeview_paradigm_settings = {
 ************************************/
 
 /**
+ * @data: the data to store
+ * @return: an html link to the download file
+ */
+function create_download_link(el) {
+    var data = webgazer.getTrainingData();
+    var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(data));
+    el.setAttribute("href", "data:"+data);
+    el.setAttribute("download", "calibration_data.json");
+  
+}
+
+/**
+ * Function to upload data for calibration
+ */
+function upload_calibration_data(){
+    var x = document.getElementById("calibration_data");
+    var txt = "";
+    if ('files' in x) {
+        if (x.files.length == 0) {
+            txt = "Select one or more files.";
+        } else {
+            for (var i = 0; i < x.files.length; i++) {
+                txt += "<br><strong>" + (i+1) + ". file</strong><br>";
+                var file = x.files[i];
+                if ('name' in file) {
+                    txt += "name: " + file.name + "<br>";
+                }
+                if ('size' in file) {
+                    txt += "size: " + file.size + " bytes <br>";
+                }
+            }
+        }
+    } 
+    else {
+        if (x.value == "") {
+            txt += "Select one or more files.";
+        } else {
+            txt += "The files property is not supported by your browser!";
+            txt  += "<br>The path of the selected file: " + x.value; // If the browser does not support the files property, it will return the path of the selected file instead. 
+        }
+    }
+    document.getElementById("demo").innerHTML = txt;
+}
+
+
+/**
  * The only function needed to call when deploy. Simple call this function when you want to start up the program
  */
 function start_medusa(parad){
