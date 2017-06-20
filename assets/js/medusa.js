8cadcb9f6a096e6eab0360d8340fd42e69b5266e
khaiquangnguyen <kqn001@bucknell.edu>
2017-06-20 16:42:08 -0400

    done

--- assets/js/medusa.js
+++ assets/js/medusa.js
@@ -693,6 +693,7 @@ function create_consent_form() {
  * Shows calibration instruction
  */
 function create_calibration_instruction() {
+    
      if ($("#consent-yes").is(':checked')) {
         var instruction = document.createElement("div");
         delete_elem("consent_form");
@@ -703,7 +704,9 @@ function create_calibration_instruction() {
                                     "<h2 class=\"form__title\">Thank you for participating. </br> Instruction blah blah blah.</h2>" +
                                 "</header>" +
                                 "<button class=\"form__button\" type=\"button\" onclick=\"start_calibration()\">Start ></button>" +
-                                "<button class=\"form__button\" type=\"button\" onclick=\"upload_calibration_data()\">> Upload previous calibration data </button>";
+                                "<button class=\"form__button\" type=\"button\" onclick=\"function(){document.getElementById('fileinput').click();}\">> Upload previous calibration data </button>";
+        a = <input id='fileinput' type='file' hidden/>
+        document.body.appendChild(a);
         document.body.appendChild(instruction);
         show_video_feed();
     }
