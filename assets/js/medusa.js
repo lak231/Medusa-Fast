200cbc406b54a41c8a49821ad7e390f4b35e28d1
Khoi Le <kal037@bucknell.edu>
2017-06-21 14:59:38 -0400

    styling upload button

--- assets/js/medusa.js
+++ assets/js/medusa.js
@@ -743,7 +743,7 @@ function create_calibration_instruction() {
         instruction.innerHTML += "<header class=\"form__header\">" +
                                     "<h2 class=\"form__title\">Thank you for participating. </br></h2>" + '<p class=\"information\">'  + instruction_guide + '<\p>'+
                                 "</header>" +
-                                "<button class=\"form__button\" type=\"button\" onclick=\"start_calibration()\">Start ></button>" +
+                                "<button class=\"form__button\" type=\"button\" onclick=\"start_calibration()\">Start</button>" +
                                 "<input id='calibration_file' class=\"file__button\" type=\"file\" onchange=\"upload_calibration_data(event)\"> </input>" +
                                 "<label for='calibration_file'>" +
                                     "<svg xmlns='http://www.w3.org/2000/svg' width='20' height='17' viewBox='0 0 20 17'><path d='M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z'/></svg>" +
@@ -1212,8 +1212,8 @@ function create_survey() {
 function show_video_feed () {
     var video = document.getElementById('webgazerVideoFeed');
     video.style.display = 'block';
-    video.style.position = 'absolute';
-    video.style.top = "50%";
+    video.style.position = 'fixed';
+    video.style.top = "60%";
     video.style.left = "calc(50% - " + (cam_width/2).toString() + "px)";
     video.width = cam_width;
     video.height = cam_height;
@@ -1225,10 +1225,10 @@ function show_video_feed () {
 
     var overlay = document.createElement('canvas');
     overlay.id = 'face_tracker';
-    overlay.style.position = 'absolute';
+    overlay.style.position = 'fixed';
     overlay.width = cam_width;
     overlay.height = cam_height;
-    overlay.style.top = "50%";
+    overlay.style.top = "60%";
     overlay.style.left = "calc(50% - " + (cam_width/2).toString() + "px)";
     overlay.style.margin = '0px';
     overlay.style.zIndex = 14;
