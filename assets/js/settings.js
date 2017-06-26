87c8a7f2ce1b86d16a645901a2958a291aed33b7
khaiquangnguyen <kqn001@bucknell.edu>
2017-06-26 14:51:16 -0400

    done

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,5 +1,5 @@
-// var IdentityPoolId = 'us-east-2:8ee03ba3-a31d-4414-882a-a7a83917a5be';
-// var RoleArn = "arn:aws:iam::345518382834:role/Cognito_medusaUnauth_Role";
+var IdentityPoolId = 'us-east-2:8ee03ba3-a31d-4414-882a-a7a83917a5be';
+var RoleArn = "arn:aws:iam::345518382834:role/Cognito_medusaUnauth_Role";
 var background_color = "#FFFFFF";
 var font_color = "#585858";
 var dark_color = "#f2849e";
